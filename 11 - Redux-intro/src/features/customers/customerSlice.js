4cc4089056477d669cb9b558beee156c8eb254b4
Sladjana <csl.bunny.2207@gmail.com>
2024-06-14 00:40:38 +0200

    redux file structure

---
+++ 11 - Redux-intro/src/features/customers/customerSlice.js
@@ -0,0 +1,34 @@
+const initalStateCustomer = {
+  fullName: "",
+  nationaID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initalStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationaID: action.payload.nationaID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "account/updateName", payload: fullName };
+}
