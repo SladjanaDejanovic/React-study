96befde68080ac82c5dc6f1de28c269ffa7e4568
Sladjana <csl.bunny.2207@gmail.com>
2024-06-15 19:53:53 +0200

    creating store with redux toolkit

--- 11 - Redux-intro/src/store.js
+++ 11 - Redux-intro/src/store.js
@@ -1,18 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-// in createStore we pass in another argument so we can connect our middleware
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
