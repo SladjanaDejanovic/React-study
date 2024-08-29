e1f233f5747d8c978ca27f7a22a416c5a7de5caa
Sladjana <csl.bunny.2207@gmail.com>
2024-08-29 22:06:09 +0200

    server.js update

--- 12 - Manual SSR/server.js
+++ 12 - Manual SSR/server.js
@@ -3,7 +3,7 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const { renderToString } = require("react-dom/server");
 const React = require("react");
-const { render } = require("react-dom");
+// const { render } = require("react-dom");
 
 const pizzas = [
 	{
